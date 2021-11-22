e8c3f068d30ddd3d68507333e55b1047e6d6b783
JIlyushenkova <94156819+JIlyushenkova@users.noreply.github.com>
2021-11-22 15:56:13 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-# File "C:\Users\Ilyushenkova\PycharmProjects\pythonProject1\main.py", line 1
-# уравнение имеет вид a*x**2+b*x+c=0
-import math
+from devices.Device import *
 
-print('введите коэффициенты для уравнения')
-print('a*x**2+b*x+c=0')
-a = float(input('a: '))
-b = float(input('b: '))
-c = float(input('c: '))
+from device import read_line, open_device, Device
 
-discr = b ** 2 - 4 * a * c
-print('Дискриминант D = ' , discr)
-if discr > 0:
-    x1 = (-b + math.sqrt(discr)) / (2 * a)
-    x2 = (-b - math.sqrt(discr)) / (2 * a)
-    print('x1 = ', x1, 'x2 = ', x2)
-elif discr == 0:
-    x = -b / (2 * a)
-    print(x)
-else:
-    print('Корней нет')
-if discr < 0:
-    print('Дискриминант D =', discr)
-else:
-    print('Корней нет')
+if __name__ == '__main__':
+    try:
+        device: Device = open_device('/devices/dev3')
+
+
+        for i in range(3):
+            print(read_line(device))
+
+
+    except Exception as exception:
+        print(f'Error: {exception}')
